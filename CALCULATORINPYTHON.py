0a6d674d6d695bf35473960d2e981ac967cee6b0
Anumoy Saha <110199601+anumoy-saha@users.noreply.github.com>
2022-07-29 11:21:41 +0530

    Add files via upload

---
+++ CALCULATORINPYTHON.py
@@ -0,0 +1,20 @@
+print("Enter the first number")
+num1=int(input())
+print("Enter the second number")
+num2=int(input())
+print("Enter the operator")
+opt=input()
+
+if opt=='+':
+		num4=num1+num2
+		print(num4)
+		
+elif opt=='*':
+			num5=num1*num2
+			print(num5)
+elif opt=='/':
+				num6=num1/num2
+				print(num6)
+elif opt=='-':
+				num7=num1-num2
+				print(num7)
